d82b98463d5b7278d00758b8b5a1184809957c2b
Joe Masilotti <joe@masilotti.com>
2021-02-19 16:13:06 -0800

    Actually, you know, use the constant

--- Library/Sources/Model/Models/Persistence.swift
+++ Library/Sources/Model/Models/Persistence.swift
@@ -3,7 +3,7 @@ import Foundation
 struct Persistence {
     static var isVisitedStatuses: [Int: Bool] {
         get {
-            UserDefaults.standard.data(forKey: "trees.visited")
+            UserDefaults.standard.data(forKey: isVisitedStatusesKey)
                 .flatMap { try? JSONDecoder().decode([Int: Bool].self, from: $0) } ?? [:]
         }
         set {
